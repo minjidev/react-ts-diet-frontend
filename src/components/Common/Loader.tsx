ab40d44c3cc00e5817220e850a524e1149ee2b4a
minji99710 <alswl99710@gmail.com>
2023-08-01 21:29:35 +0900

    Style: Modify Loading component style
    - display in the middle

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
 const Loader = () => {
-  return <>Loading...</>;
+  return (
+    <Container>
+      <Loading>Loading...</Loading>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const Loading = styled.div`
+  padding-bottom: 4rem;
+`;
 
 export default Loader;
