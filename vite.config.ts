01d52a8ab471047f2f22e9ec4792310bf0a92481
minji99710 <alswl99710@gmail.com>
2023-09-06 21:35:29 +0900

    Refactor: Remove proxy
    - Use cors in Express instead

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,4 @@ const PORT = 8000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-    },
-  },
 });
