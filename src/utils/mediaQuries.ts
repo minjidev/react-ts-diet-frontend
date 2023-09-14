471f603856d70f32f400567ba70e6a990c9345dc
minji99710 <alswl99710@gmail.com>
2023-09-14 22:33:01 +0900

    Feat: Add mediaQuery for mobile and tablet

---
+++ src/utils/mediaQuries.ts
@@ -0,0 +1,7 @@
+const mobile = 480;
+const tablet = 768;
+
+const mobileQuery = `@media (max-width: ${mobile}px)`;
+const tabletQuery = `@media (max-width: ${tablet}px)`;
+
+export { mobileQuery, tabletQuery };
