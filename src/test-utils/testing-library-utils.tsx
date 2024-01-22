1b8f4d6ccdf514acf639d2228f39f5e4f6251f9e
minji99710 <alswl99710@gmail.com>
2024-01-22 21:28:26 +0900

    Feat: Add setup for test
    - Add intersectionObserver to vitest global
    - Modify defaultOptions of React QueryClient to use Suspense and ErrorBoudary

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const createWrapper = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+        useErrorBoundary: true,
+        retry: false,
+        cacheTime: 0,
+      },
+    },
+  });
 
   return function ({ children }: { children: React.ReactNode }) {
     return (
@@ -19,12 +28,17 @@ const createWrapper = () => {
 };
 
 type RenderParams = Parameters<typeof render>;
+type RenderHookParams = Parameters<typeof renderHook>;
 
 const renderWithContext = (ui: RenderParams[0], options?: RenderParams[1]) =>
   render(ui, { wrapper: createWrapper(), ...options });
+
+const renderHookWithContext = (hook: RenderHookParams[0], options?: RenderHookParams[1]) =>
+  renderHook(hook, { wrapper: createWrapper(), ...options });
 
 // re-export everything
 export * from '@testing-library/react';
 
 // overried render method
 export { renderWithContext as render };
+export { renderHookWithContext as renderHook };
--- src/tools/tests/setupTests.ts
+++ src/tools/tests/setupTests.ts
@@ -1,8 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import '@testing-library/jest-dom';
-import { beforeAll, afterEach, afterAll } from 'vitest';
+import { beforeAll, afterEach, afterAll, vi } from 'vitest';
 import { server } from './server';
+
+const mockedIntersectionObserver = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserver: vi.fn(),
+}));
+
+vi.stubGlobal('IntersectionObserver', mockedIntersectionObserver);
 
 beforeAll(() => server.listen());
 
