d4b848ccc4fa7253631f350492610711e33380c0
minji99710 <alswl99710@gmail.com>
2024-02-06 15:41:59 +0900

    Feat: Add ErrorFallback test for Render
    - Check if it displays correct error message based on props and error status

--- src/components/__tests__/ErrorFallback.test.tsx
+++ src/components/__tests__/ErrorFallback.test.tsx
@@ -1,42 +1,63 @@
 import React from 'react';
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import * as reactErrorBoundary from 'react-error-boundary';
+import { useErrorBoundary } from 'react-error-boundary';
 import { render, screen } from '../../test-utils/testing-library-utils';
 import ErrorFallback from '../common/ErrorFallback';
 
+// mock useErrorBoundary
+vi.mock('react-error-boundary', () => ({
+  useErrorBoundary: vi.fn(),
+}));
+
 describe('#ErrorFallback', () => {
-  describe('#Event', () => {
-    // mock useErrorBoundary
-    vi.mock('react-error-boundary', async importOriginal => {
-      const mod = await importOriginal<typeof import('react-error-boundary')>();
-      return {
-        ...mod,
-        useErrorBoundary: vi
-          .fn()
-          .mockReturnValue({ resetBoundary: vi.fn(), showBoundary: vi.fn() }),
-      };
+  const error = {
+    name: 'CustomError',
+    message: 'An error occurred',
+    response: {
+      data: {},
+      status: 500,
+      headers: '',
+    },
+  };
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+    vi.mocked(useErrorBoundary).mockReturnValue({
+      resetBoundary: vi.fn(),
+      showBoundary: vi.fn(),
+    });
+  });
+
+  describe('#Render', () => {
+    it('displays the corresponding error message based showFullErrorMessage pro', () => {
+      // displays error message based on error status if showFullErrorMessage is true
+      const { rerender } = render(<ErrorFallback error={error} showFullErrorMessage />);
+      const errorMessage = screen.getByText(/Something went wrong on our end/i);
+      expect(errorMessage).toBeInTheDocument();
+
+      // displays default error message if showFullErrorMessage is false
+      rerender(<ErrorFallback error={error} showFullErrorMessage={false} />);
+      const defaultErrorMessage = screen.getByText(/Please Try Again/i);
+      expect(defaultErrorMessage).toBeInTheDocument();
     });
 
-    it.only('calls resetBoundary when the Retry button is clicked.', async () => {
+    it('displays the default error message', () => {
+      render(<ErrorFallback error={error} showFullErrorMessage />);
+      const errorMessage = screen.getByText(/Something went wrong on our end/i);
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  describe('#Event', () => {
+    it('calls resetBoundary when the Retry button is clicked.', async () => {
       const user = userEvent.setup();
-
-      const error = {
-        name: 'CustomError',
-        message: '',
-        response: {
-          data: {},
-          status: 500,
-          headers: '',
-        },
-      };
-
       render(<ErrorFallback error={error} />);
 
       const retryButton = screen.getByRole('button', { name: /retry/i });
       await user.click(retryButton);
 
-      expect(reactErrorBoundary.useErrorBoundary().resetBoundary).toHaveBeenCalled();
+      expect(useErrorBoundary().resetBoundary).toHaveBeenCalled();
     });
   });
 });
