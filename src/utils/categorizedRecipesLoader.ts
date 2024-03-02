8d224a299dfd65f33f361ed160c3e63dfef130cb
minji99710 <alswl99710@gmail.com>
2024-03-02 20:25:36 +0900

    Refactor: Modify categorizedRecipesLoader to support React Suspense by implementing render-as-you-fetch pattern
    - Remove async-await and return null

--- src/utils/categorizedRecipesLoader.ts
+++ src/utils/categorizedRecipesLoader.ts
@@ -2,16 +2,16 @@ import { QueryClient } from '@tanstack/react-query';
 import { categorizedRecipesQuery } from './query/categorizedRecipesQuery';
 import { categories } from '../constants/index';
 
-const categorizedRecipesLoader = (queryClient: QueryClient) => async () => {
-  const categorizedRecipes = await Promise.all(
-    categories.map(async (category: string) => {
+const categorizedRecipesLoader = (queryClient: QueryClient) => () => {
+  Promise.all(
+    categories.map((category: string) => {
       const query = categorizedRecipesQuery(category);
 
       return queryClient.ensureQueryData(query);
     }),
   );
 
-  return categorizedRecipes;
+  return null;
 };
 
 export default categorizedRecipesLoader;
