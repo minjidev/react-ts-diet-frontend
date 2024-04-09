dfdb701a361c4d7664028adb040fc2b826550d74
minji99710 <alswl99710@gmail.com>
2024-04-09 18:46:05 +0900

    Test cicd

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { AboutContent } from '../components/index';
+import TestComponent from '../components/TestComponent';
 
 const About = () => {
-  return <AboutContent />;
+  return (
+    <div>
+      <AboutContent />
+      <TestComponent />
+    </div>
+  );
 };
 
 export default About;
